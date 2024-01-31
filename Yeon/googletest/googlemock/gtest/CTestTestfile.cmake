5127249c58e4dac186e5b0caf73fc40942d7622a
YeonHui829 <dusgml8829@gmail.com>
2024-01-31 10:27:38 +0900

    [MERGE]

--- Yeon/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/s22100141/study_2024/Fuzz_Fshare/googletest/googletest
-# Build directory: /home/s22100141/study_2024/Fuzz_Fshare/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

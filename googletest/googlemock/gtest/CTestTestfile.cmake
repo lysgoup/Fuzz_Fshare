21068ebb31c385c8bb8be346159984d7de44f5d5
YeonHui829 <dusgml8829@gmail.com>
2024-01-31 09:51:54 +0900

    [UPDATE] Making check directory fuzz main, input file

---
+++ googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/s22100141/study_2024/Fuzz_Fshare/googletest/googletest
+# Build directory: /home/s22100141/study_2024/Fuzz_Fshare/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
